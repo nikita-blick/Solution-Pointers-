ab40e9b878c3c6a2e6e480acff48b515984be07e
Nikita <Nikita@DESKTOP-13IEN5T>
2025-06-16 21:39:50 +0500

    mster DONE

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -20,10 +20,25 @@ int* push_front(int arr[], int& n, const int value);
 int* pop_back(int arr[], int& n);
 int* pop_front(int arr[], int& n);
 
+int* insert(int arr[], int& n, const int value, const int index);
+int* erase(int arr[], int& n, const int index);
+
 int** push_row_back(int** arr, int& rows, const int cols);
-int** insert_row(int** arr, int& rows, const int cols, const int index);
+int** push_row_front(int** arr, int& rows, const int cols);
+int** pop_row_back(int** arr, int& rows);
+int** pop_row_front(int** arr, int& rows);
 
 void push_col_back(int** arr, const int rows, int& cols);
+void push_col_front(int** arr, const int rows, int& cols);
+void pop_col_back(int** arr, const int rows, int& cols);
+void pop_col_front(int** arr, const int rows, int& cols);
+
+int** insert_row(int** arr, int& rows, const int cols, const int index);
+int** erase_row(int** arr, int& rows, const int index);
+
+void insert_col(int** arr, const int rows, int& cols, const int index);
+void erase_col(int** arr, const int rows, int& cols, const int index);
+
 
 //#define DYNAMIC_MEMORY_1
 #define DYNAMIC_MEMORY_2
@@ -234,43 +249,80 @@ int* pop_front(int arr[], int& n)
 	return buffer;
 }
 
+int* insert(int arr[], int& n, const int value, const int index)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n + 1; i++)
+	{
+		if (i < index)buffer[i] = arr[i];
+		else if (i == index)buffer[i] = value;
+		else buffer[i] = arr[i - 1];
+	}
+	delete[] arr;
+	n++;
+	return buffer;
+	
+}
+int* erase(int arr[], int& n, const int index)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (i < index) buffer[i] = arr[i];
+		else buffer[i] = arr[i + 1];
+	}
+	delete[] arr;
+	n--;
+	return buffer;
+	
+}
+
 int** push_row_back(int** arr, int& rows, const int cols)
 {
-	//1) создаем буферный массив указателей нужного размера:
+	//1) Создаём буфферный массив указателей нужного размера: 
 	int** buffer = new int* [rows + 1];
 
-	//2) копируем адреса строк в новый массив:
+	//2) Копируем адреса строк в новый массив: 
 	for (int i = 0; i < rows; i++)
 	{
 		buffer[i] = arr[i];
 	}
 
-	//3) удаляем исходный массив указателейЖ
+	//3) Удаляем исходный массив указателей:
 	delete[] arr;
 
-	//4) добавляем добавляемую строку:
+	//4) Добавляем добавляемую строку:
 	buffer[rows] = new int[cols] {};
 
-	//5) после добавления строки, кол-во строк увеличивается на 1:
+	//5) После добавления строки, количество строк увеличивается на 1:
 	rows++;
 
-	//6) возвращаем новый массив:
+	//6) Возвращаем новый массив:
 	return buffer;
 }
-
-int** insert_row(int** arr, int& rows, const int cols, const int index)
+int** push_row_front(int** arr, int& rows, const int cols)
 {
-	if (index<0 || index>rows)
-	{
-		cout << "Error: Out of range exception" << endl;
-		return arr;
-	}
-	int** buffer = new int* [rows + 1] {};
-	for (int i = 0;i < index;i++)buffer[i] = arr[i];
-	for (int i = index;i < rows; i++)buffer[i + 1] = arr[i];
+	int** buffer = new int* [rows + 1];
+	buffer[0] = new int[cols] {};
+	for (int i = 0; i < rows; i++)buffer[i + 1] = arr[i];
 	delete[] arr;
-	buffer[index] = new int[cols] {};
 	rows++;
+	return buffer;
+}
+int** pop_row_back(int** arr, int& rows)
+{
+	delete[] arr[rows - 1];
+	int** buffer = new int* [--rows];
+	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+int** pop_row_front(int** arr, int& rows)
+{
+	delete[] arr[0];
+	int** buffer = new int* [--rows];
+	for (int i = 0; i < rows; i++) buffer[i] = arr[i + 1];
+	delete[] arr;
 	return buffer;
 }
 
@@ -278,14 +330,111 @@ void push_col_back(int** arr, const int rows, int& cols)
 {
 	for (int i = 0; i < rows; i++)
 	{
-		//1) создаем буферную строку нужного размера:
+		//1) Создаём буферную строку нужного размера: 
 		int* buffer = new int[cols + 1] {};
+
 		//2) копируем элементы из исходной строки в буферную:
 		for (int j = 0; j < cols; j++)buffer[j] = arr[i][j];
-		//3) удаляем исходную строку:
+
+		//3) Удаляем сходную строку:
 		delete[] arr[i];
-		//4) подменяем адрес исходной строки адресом новой строки:
+
+		//4) Подменяем адрес исходной строки адресом новой строки:
 		arr[i] = buffer;
 	}
 	cols++;
+}
+void push_col_front(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols + 1] {};
+		for (int j = 0; j < cols; j++)buffer[j + 1] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols++;
+}
+void pop_col_back(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols - 1];
+		for (int j = 0; j < cols - 1; j++)buffer[j] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols--;
+}
+void pop_col_front(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols - 1];
+		for (int j = 0; j < cols - 1; j++)buffer[j] = arr[i][j + 1];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols--;
+}
+
+int** insert_row(int** arr, int& rows, const int cols, const int index)
+{
+	if (index < 0 || index>rows)
+	{
+		cout << "Error: Out of range exception" << endl;
+		return arr;
+	}
+	int** buffer = new int* [rows + 1];
+	for (int i = 0; i < rows + 1; i++)
+	{
+		buffer[i] = (i < index ? arr[i] : (i == index) ? new int[cols] {} : arr[i - 1]);
+	}
+
+	delete[] arr;
+	rows++;
+	return buffer;
+}
+
+int** erase_row(int** arr, int& rows, const int index)
+{
+	delete[] arr[index];
+	int** buffer = new int* [--rows];
+	for (int i = 0; i < index; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	for (int i = index; i < rows; i++)
+	{
+		buffer[i] = arr[i + 1];
+	}
+
+	delete[] arr;
+	return buffer;
+}
+
+void insert_col(int** arr, const int rows, int& cols, const int index)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols + 1] {};
+		for (int j = 0; j < index; j++) buffer[j] = arr[i][j];
+		buffer[index] = 0;
+		for (int j = index; j < cols + 1; j++) buffer[j + 1] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols++;
+}
+void erase_col(int** arr, const int rows, int& cols, const int index)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols - 1];
+		for (int j = 0; j < index; j++)buffer[j] = arr[i][j];
+		for (int j = index; j < cols - 1; j++)buffer[j] = arr[i][j + 1];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols--;
 }
